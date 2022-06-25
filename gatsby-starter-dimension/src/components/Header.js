c627e792b5e3d2ffd8bf1ff4cfce324caf89fde8
kabinja <renaudrwemalika@gmail.com>
2022-06-25 15:51:45 +0200

    first commit

---
+++ gatsby-starter-dimension/src/components/Header.js
@@ -0,0 +1,64 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import logo from '../images/rwemalika.png'
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+      <img src={logo} alt="" className="logo"></img>
+    <div className="content">
+      <div className="inner">
+        <h1>Renaud Rwemalika Ph.D.</h1>
+        <p>
+        Living in a world where the complexiy of systems is always incresing while they rely on more and more on data, the most exciting challenge of our generation is to build the tools to support those advancement. This is why I have dedicated my carrier on complex systems quality and data engineering.
+        </p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('resume')
+            }}
+          >
+            resume
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('projects')
+            }}
+          >
+            projects
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('papers')
+            }}
+          >
+            papers
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            contact
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+}
+
+export default Header
